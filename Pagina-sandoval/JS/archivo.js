74ffdbada2bf8919c64f6c393ddc5f22ead0bfe8
carlonchosando <cdscapitolio@gmail.com>
2021-07-05 01:10:49 -0300

    grabado del domingo 5-07-21

---
+++ Pagina-sandoval/JS/archivo.js
@@ -0,0 +1,10 @@
+recetaCompleta = [];
+
+function completaLaReceta() {
+    recetaCompleta = [];
+    for (var i = 1; i <= 5; i++) {
+      ingrediente = prompt("Ingresa tu ingrediente Nº " + i + ":");
+      recetaCompleta.push( ingrediente ); 
+    }  
+    document.getElementById("muestra_ingrediente").innerHTML = recetaCompleta.sort();  
+}
