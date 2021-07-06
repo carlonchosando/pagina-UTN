b30cd43879b35c944f25b116c993c28dec505bc9
carlonchosando <cdscapitolio@gmail.com>
2021-07-06 06:20:36 -0300

    demas botones

--- JS/reproductor.js
+++ JS/reproductor.js
@@ -2,5 +2,13 @@ $(document).ready(function() {
     $('#iniciaContinua').click(function(){
         var video = $('video')[0]
         video.play();
-    })
-})
+    });
+    $('#para').click(function(){
+        var video = $('video')[0]
+        video.pause();
+        video.currentTime = 0;
+    });
+    $('#avanza').click(function(){
+        video.currentTime = video.currentTime + 1.3;
+    });
+});
